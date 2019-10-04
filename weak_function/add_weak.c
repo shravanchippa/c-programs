7a8f41b78d513ea6e218b235fef613c68371e2e6
shravan kumar <shravan.chippa@microchip.com>
2019-10-04 10:42:34 +0530

    weak function example
    Signed-off-by: shravan kumar <shravan.chippa@microchip.com>

---
+++ weak_function/add_weak.c
@@ -0,0 +1,4 @@
+int __attribute__((weak)) add(int x,int y)
+{
+	return 0;
+}
---
+++ weak_function/weak.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+extern int add(int x,int y);
+
+void main(void)
+{
+	int i=3, j=4;
+	printf("weK function example add i+j = %d \n", add(i,j));
+	
+
+}
